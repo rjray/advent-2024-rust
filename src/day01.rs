80160484e76a7a76518ccbf21d372a66d10c36aa
Randy J. Ray <rjray@blackperl.com>
2024-12-01 16:21:16 -0800

    Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,9 +1,44 @@
 // Day 1
 
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+pub fn get_column_numbers(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|e| e.parse::<i32>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip()
+}
+
 pub fn part1(input: String) {
-    println!("Not implemented yet");
+    let (mut left, mut right) = get_column_numbers(&input);
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let value: i32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(&l, &r)| (l - r).abs())
+        .sum();
+
+    println!("{}", value);
 }
 
 pub fn part2(input: String) {
-    println!("Not implemented yet");
+    let (left, right) = get_column_numbers(&input);
+
+    let mut freqs = HashMap::new();
+    for val in right {
+        *freqs.entry(val).or_insert(0) += 1;
+    }
+
+    let value: i32 = left.iter().map(|v| freqs.get(v).unwrap_or(&0) * v).sum();
+
+    println!("{}", value);
 }
